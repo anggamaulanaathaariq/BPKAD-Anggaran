5d869ffb4fbc8fdb75416222068eac56c691da64
Angga Maulana Athaariq <am.athaariq@gmail.com>
2021-01-04 15:19:36 +0700

    update db

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -1,4 +1,4 @@
 <?php
 // isi nama host, username mysql, dan password mysql anda
-$koneksi = new mysqli('localhost', 'root', '', 'db_moora');
+$koneksi = new mysqli('localhost', 'root', '', 'bpkad_bidang_anggaran');
 ?>
